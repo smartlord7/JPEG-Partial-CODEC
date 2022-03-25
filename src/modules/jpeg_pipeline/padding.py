5ad245230a350f7d3f5dfa0de91c29c6cb9a2b23
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-03-25 19:50:56 +0000

    MASTER: Improve padding.py.

--- src/modules/jpeg_pipeline/padding.py
+++ src/modules/jpeg_pipeline/padding.py
@@ -1,4 +1,4 @@
-"""------------DESTRUCTIVE COMPRESSION OF IMAGE - PARTIAL JPEG CODEC------------
+"""------------DESTRUCTIVE COMPRESSION OF matrix - PARTIAL JPEG CODEC------------
 University of Coimbra
 Degree in Computer Science and Engineering
 Multimedia
@@ -10,21 +10,22 @@ Tiago Filipe Santa Ventura, 2019243695, uc2019243695@student.uc.pt
 Coimbra, 23rd March 2022
 ---------------------------------------------------------------------------"""
 
+
 import cv2
 import numpy as np
 
 
-def apply_padding(image: np.ndarray, wanted_rows: int, wanted_cols: int, interpolation_type: int = None):
+def apply_padding(matrix: np.ndarray, wanted_rows: int, wanted_cols: int, interpolation_type: int = None):
     """
-                            Applies padding to the image.
-                            :param image: the image to pad.
-                            :param wanted_rows: number of rows to pad.
-                            :param wanted_cols: number of columns to pad.
-                            :param interpolation_type: the chosen interpolation.
-                            :return: the image with padding.
+    Applies padding to the matrix.
+    :param matrix: the matrix to pad.
+    :param wanted_rows: number of rows that the matrix height must be multiple of.
+    :param wanted_cols: number of columns that the matrix height must be multiple of.
+    :param interpolation_type: the chosen interpolation (e.g. LINEAR; CUBIC; AREA).
+    :return: the padded matrix.
     """
-    n_rows = image.shape[0]
-    n_cols = image.shape[1]
+    n_rows = matrix.shape[0]
+    n_cols = matrix.shape[1]
 
     if interpolation_type is not None:
         remaining_rows = int()
@@ -35,51 +36,51 @@ def apply_padding(image: np.ndarray, wanted_rows: int, wanted_cols: int, interpo
         if wanted_cols != 0:
             remaining_cols = wanted_cols - n_cols % wanted_cols
 
-        image = cv2.resize(image, (n_cols + remaining_cols, n_rows + remaining_rows), interpolation_type)
+        matrix = cv2.resize(matrix, (n_cols + remaining_cols, n_rows + remaining_rows), interpolation_type)
     else:
         if wanted_rows != 0:
             remaining = n_rows % wanted_rows
 
             if remaining != 0:
                 remaining = wanted_rows - remaining
-                last_row = image[n_rows - 1]
+                last_row = matrix[n_rows - 1]
                 rows_to_add = np.repeat([last_row], remaining, axis=0)
-                image = np.vstack((image, rows_to_add))
+                matrix = np.vstack((matrix, rows_to_add))
 
         if wanted_cols != 0:
             remaining = n_cols % wanted_cols
 
             if remaining != 0:
                 remaining = wanted_cols - remaining
-                last_col = image[:, [-1]]
-                image = np.hstack((image, np.tile(last_col, (remaining, 1))))
+                last_col = matrix[:, [-1]]
+                matrix = np.hstack((matrix, np.tile(last_col, (remaining, 1))))
 
-    return image
+    return matrix
 
 
-def inverse_padding(padded_image: np.ndarray, original_rows: int, original_cols: int, interpolation_type: int = None):
+def inverse_padding(padded_matrix: np.ndarray, original_rows: int, original_cols: int, interpolation_type: int = None):
     """
-                                Reverses the padding.
-                                :param padded_image: the padded image to unpad.
-                                :param original_rows: number of original rows.
-                                :param original_cols: number of original columns.
-                                :param interpolation_type: the chosen interpolation.
-                                :return: the original image.
+    Reverses the padding.
+    :param padded_matrix: the padded matrix to unpad.
+    :param original_rows: number of original rows.
+    :param original_cols: number of original columns.
+    :param interpolation_type: the chosen interpolation (e.g. LINEAR; CUBIC; AREA).
+    :return: the original matrix.
     """
-    n_rows = padded_image.shape[0]
-    n_cols = padded_image.shape[1]
+    n_rows = padded_matrix.shape[0]
+    n_cols = padded_matrix.shape[1]
 
     if interpolation_type is not None:
-        return cv2.resize(padded_image, (original_cols, original_rows), interpolation_type)
+        return cv2.resize(padded_matrix, (original_cols, original_rows), interpolation_type)
     else:
         n_rows_to_delete = n_rows - original_rows
         if n_rows_to_delete != 0:
             rows_to_delete = np.arange(n_rows - n_rows_to_delete - 1, n_rows - 1)
-            padded_image = np.delete(padded_image, rows_to_delete, axis=0)
+            padded_matrix = np.delete(padded_matrix, rows_to_delete, axis=0)
 
         n_cols_to_delete = n_cols - original_cols
         if n_cols_to_delete != 0:
             cols_to_delete = np.arange(n_cols - n_cols_to_delete - 1, n_cols - 1)
-            padded_image = np.delete(padded_image, cols_to_delete, axis=1)
+            padded_matrix = np.delete(padded_matrix, cols_to_delete, axis=1)
 
-        return padded_image
+        return padded_matrix
